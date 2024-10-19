7b3ff0da6105bc943e8bfa2d10fb76cdcafbdcb0
Tom Fre <tftomsun@hotmail.de>
2024-10-19 11:02:25 +0200

    added basic gantt chart sample

---
+++ src/GantChartDemo.App/GantChartDemo.App/wwwroot/GantChartInterop.js
@@ -0,0 +1,55 @@
+﻿import * as charts from "./packages/apexcharts/dist/apexcharts.esm.js"
+export function createGantChart(element, options) {
+
+    const finalOptions = options ?? {
+        series: [
+            {
+                data: [
+                    {
+                        x: 'Code',
+                        y: [
+                            new Date('2019-03-02').getTime(),
+                            new Date('2019-03-04').getTime()
+                        ]
+                    },
+                    {
+                        x: 'Test',
+                        y: [
+                            new Date('2019-03-04').getTime(),
+                            new Date('2019-03-08').getTime()
+                        ]
+                    },
+                    {
+                        x: 'Validation',
+                        y: [
+                            new Date('2019-03-08').getTime(),
+                            new Date('2019-03-12').getTime()
+                        ]
+                    },
+                    {
+                        x: 'Deployment',
+                        y: [
+                            new Date('2019-03-12').getTime(),
+                            new Date('2019-03-18').getTime()
+                        ]
+                    }
+                ]
+            }
+        ],
+        chart: {
+            height: 350,
+            type: 'rangeBar'
+        },
+        plotOptions: {
+            bar: {
+                horizontal: true
+            }
+        },
+        xaxis: {
+            type: 'datetime'
+        }
+    };
+    const chart = new charts.default(element, finalOptions);
+ 
+    return chart;
+}
